57044aea3d7bf0da38465981aaab14d82127b2eb
AmirHosseinHedayati <amir.0098.14@gmail.com>
2020-08-08 19:32:32 +0430

    delete class Sprite and add Slot (switchpic) for making the zombie walk in class Zombie

--- Zombie.cpp
+++ Zombie.cpp
@@ -1,6 +1,43 @@
 #include "Zombie.h"
 
-Zombie::Zombie(QObject *parent) : QObject(parent)
+Zombie::Zombie(const int& pixPer50MiliSec) : QObject(), QGraphicsPixmapItem()
 {
 
+currentPic =0;
+
+this->pixPer50MiliSec = pixPer50MiliSec;
+//setPixmap(QPixmap(":/pictures/1.png"));
+
+picTimer = new QTimer();
+connect(picTimer,SIGNAL(timeout()),this , SLOT(switchPic()));
+picTimer->start(300);
+
+zTimer = new QTimer();
+connect(zTimer ,SIGNAL(timeout()), this , SLOT(movetoLeft()));
+zTimer->start(50);
+
+
+normPicZombieList<<QPixmap(":/pictures/1.png")<<QPixmap(":/pictures/2.png")<<
+QPixmap(":/pictures/3.png")<<QPixmap(":/pictures/4.png")<<
+QPixmap(":/pictures/5.png");
+}
+
+void Zombie::movetoLeft()
+{
+    setPos(x()-pixPer50MiliSec,y());
+
+    if(x()==0){
+
+    }
+}
+
+void Zombie::switchPic()
+{
+    if(currentPic != normPicZombieList.size()){
+    setPixmap(QPixmap(normPicZombieList[currentPic]));
+    currentPic++;
+    }
+    else {
+        currentPic =0;
+}
 }
--- Zombie.h
+++ Zombie.h
@@ -2,16 +2,25 @@
 #define ZOMBIE_H
 
 #include <QObject>
-
-class Zombie : public QObject
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <QList>
+class Zombie : public QObject , public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
-    explicit Zombie(QObject *parent = nullptr);
-
-signals:
+    Zombie(const int& pixPer50MiliSec);
+private:
+    QTimer *zTimer;
+    QTimer *picTimer;
+    int life;
+    int pixPer50MiliSec;
+    QList <QPixmap> normPicZombieList;
+    int currentPic;
 
 public slots:
+    void movetoLeft();
+    void switchPic();
 };
 
 #endif // ZOMBIE_H
