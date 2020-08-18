f7d6830d8835f08366fd997b51c091daaa07da87
AmirHosseinHedayati <amir.0098.14@gmail.com>
2020-08-18 19:09:43 +0430

    imrove class Zombie

--- Zombie.cpp
+++ Zombie.cpp
@@ -1,43 +1,94 @@
 #include "Zombie.h"
+#include <QGraphicsScene>
+#include <QList>
+#include "Plant.h"
+#include "Sun.h"
+#include "Sun2.h"
+#include "Potatoplant.h"
+#include "Button.h"
 
-Zombie::Zombie(const int& pixPer50MiliSec) : QObject(), QGraphicsPixmapItem()
+Zombie::Zombie(const int&life, const int& pixPer50MiliSec, QTimer *timer , QGraphicsItem *parent) : QObject(),
+QGraphicsPixmapItem(parent), life{life},pixPer50MiliSec {pixPer50MiliSec}
 {
-
 currentPic =0;
+timeIntervals =0;
 
-this->pixPer50MiliSec = pixPer50MiliSec;
-//setPixmap(QPixmap(":/pictures/1.png"));
+//connect timer to slot (switchpic)
+connect(timer,SIGNAL(timeout()),this , SLOT(switchPic()));
 
-picTimer = new QTimer();
-connect(picTimer,SIGNAL(timeout()),this , SLOT(switchPic()));
-picTimer->start(300);
+//connect timer to moveToLeft
+connect(timer ,SIGNAL(timeout()), this , SLOT(movetoLeft()));
 
-zTimer = new QTimer();
-connect(zTimer ,SIGNAL(timeout()), this , SLOT(movetoLeft()));
-zTimer->start(50);
+//add music for zombie
+musicPlayer = new QMediaPlayer();
+musicPlayer->setMedia(QUrl("qrc:/music/groan.mp3"));
+musicPlayer->play();
 
+//add music when you lose
+loseMusicPlayer = new QMediaPlayer();
+loseMusicPlayer->setMedia(QUrl("qrc:/music/lose.mp3"));
 
-normPicZombieList<<QPixmap(":/pictures/1.png")<<QPixmap(":/pictures/2.png")<<
-QPixmap(":/pictures/3.png")<<QPixmap(":/pictures/4.png")<<
-QPixmap(":/pictures/5.png");
+//set graphic
+normPicZombieList<<QPixmap(":/pictures/z1.png")<<QPixmap(":/pictures/z2.png")<<QPixmap(":/pictures/z3.png")
+                <<QPixmap(":/pictures/z4.png")<<QPixmap(":/pictures/z5.png")<<QPixmap(":/pictures/z6.png")
+                   <<QPixmap(":/pictures/z7.png")<<QPixmap(":/pictures/z8.png")<<QPixmap(":/pictures/z9.png")
+                   <<QPixmap(":/pictures/z10.png")<<QPixmap(":/pictures/z11.png")<<QPixmap(":/pictures/z12.png")
+                     <<QPixmap(":/pictures/z13.png")<<QPixmap(":/pictures/z14.png")<<QPixmap(":/pictures/z15.png");
+}
+
+Zombie::~Zombie()
+{
+    delete musicPlayer;
+}
+
+void Zombie::decrementLife()
+{
+    life--;
+    if(life==0){
+        scene()->removeItem(this);
+        delete this;
+    }
+}
+
+void Zombie::destroy()
+{
+    scene()->removeItem(this);
+    delete this;
 }
 
 void Zombie::movetoLeft()
 {
+
+    QList<QGraphicsItem *>collidingObjects = collidingItems();
+
+    for(int i =0 ; i <collidingObjects.size() ; i++){
+            Plant *plant =dynamic_cast<Plant*>(collidingObjects[i]);
+            PotatoPlant *potato = dynamic_cast<PotatoPlant*>(collidingObjects[i]); 
+            if(plant && plant!=potato){
+                scene()->removeItem(collidingObjects[i]);
+                delete collidingObjects[i];
+            }
+        }
     setPos(x()-pixPer50MiliSec,y());
-
     if(x()==0){
-
+      loseMusicPlayer->play();
+      exit(1);
     }
 }
 
 void Zombie::switchPic()
 {
+    ++timeIntervals;
+    if(timeIntervals %7 ==0){
     if(currentPic != normPicZombieList.size()){
     setPixmap(QPixmap(normPicZombieList[currentPic]));
     currentPic++;
     }
     else {
         currentPic =0;
+    }
+  }
 }
-}
+
+
+
--- Zombie.h
+++ Zombie.h
@@ -5,18 +5,28 @@
 #include <QGraphicsPixmapItem>
 #include <QTimer>
 #include <QList>
+#include <QMediaPlayer>
+#include <QGraphicsScene>
 class Zombie : public QObject , public QGraphicsPixmapItem
 {
     Q_OBJECT
+
 public:
-    Zombie(const int& pixPer50MiliSec);
+    Zombie(const int&life, const int& pixPer50MiliSec, QTimer *timer ,  QGraphicsItem *parent);
+    ~Zombie();
+    void decrementLife();
+    void destroy();
+
 private:
-    QTimer *zTimer;
     QTimer *picTimer;
     int life;
     int pixPer50MiliSec;
     QList <QPixmap> normPicZombieList;
     int currentPic;
+    QMediaPlayer *musicPlayer;
+    QMediaPlayer *loseMusicPlayer;
+    int timeIntervals;
+
 
 public slots:
     void movetoLeft();
